891796f7bb1f20ea1e335ae904059b7d8a888f96
wyatt_was <127136122+wyattwas@users.noreply.github.com>
2023-07-03 21:57:58 +0000

    changing User user to String userId

--- src/main/java/main/Ticket.java
+++ src/main/java/main/Ticket.java
@@ -2,41 +2,35 @@ package main;
 
 import java.io.Serializable;
 
-import net.dv8tion.jda.api.entities.User;
-
 public class Ticket implements Serializable {
-    private String solved;
+    private Boolean solved;
     private Integer ticketId;
-    private String user;
+    private String userId;
     private String topic;
     private String message;
 
-    public Ticket(Boolean lsolved, Integer lticketId, User luser, String ltopic, String lmessage) {
-        solved = lsolved.toString();
+    public Ticket(Boolean lsolved, Integer lticketId, String luserId, String ltopic, String lmessage) {
+        solved = lsolved;
         ticketId = lticketId;
-        user = luser.toString();
+        userId = luserId;
         topic = ltopic;
         message = lmessage;
     }
 
     void ticketSetSolvedTrue() {
-        solved = "true";
+        solved = true;
     }
 
     public Boolean getSolved() {
-        if (solved.equals("false")) {
-            return false;
-        } else {
-            return true;
-        }
+        return solved;
     }
 
     public Integer getTicketId() {
         return ticketId;
     }
 
-    public String getUser() {
-        return user;
+    public String getUserId() {
+        return userId;
     }
 
     public String getTopic() {
--- src/main/java/main/support.java
+++ src/main/java/main/support.java
@@ -1,11 +1,8 @@
 package main;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +11,7 @@ import javax.annotation.Nonnull;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
@@ -23,12 +21,15 @@ import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.Modal;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.JDA;
 
 public class support extends ListenerAdapter {
 
     Integer ticketId;
+    JDA jda;
 
     @Override
     public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
@@ -60,35 +61,65 @@ public class support extends ListenerAdapter {
             Map<Integer, Ticket> map = new HashMap<Integer, Ticket>();
             EmbedBuilder emb = new EmbedBuilder();
             try {
-                map = mapper.readValue(new File("tickets.json"), new TypeReference<HashMap<Integer, Ticket>>() {
+                map = mapper.readValue(new File("tickets.json"), new TypeReference<Map<Integer, Ticket>>() {
                 });
                 if (event.getOption("ticket-id") != null) {
-                    Integer lticketId = event.getOption("ticket-id").getAsInt();
-                    for (Ticket value : map.values()) {
-                        if (value.getTicketId() == lticketId) {
-                            emb.addField("Ticket ID", lticketId.toString(), false);
-                            emb.addField("User", value.getUser(), false);
-                            emb.addField("Topic", value.getTopic(), false);
-                            emb.addField("Message", value.getMessage(), false);
+                    Integer ticketId = event.getOption("ticket-id").getAsInt();
+                    if (event.getOption("closeticket") != null) {
+                        Boolean closeticket = event.getOption("closeticket").getAsBoolean();
+                        if (closeticket == true) {
+                            for (Ticket value : map.values()) {
+                                if (value.getTicketId() == ticketId) {
+                                    value.ticketSetSolvedTrue();
+                                    String userId = value.getUserId();
+                                    User user = jda.getUserById(userId);
+                                    user.openPrivateChannel().flatMap(channel -> channel.sendMessage(user
+                                            + " your support form with the ID **" + ticketId
+                                            + "** has been marked as closed. The problem should be solved now. If this is not the case, please contact a support member or open a new ticket under the same ticket ID."))
+                                            .queue();
+                                    event.reply("The ticket with the ID **" + ticketId + "** has been marked as closed")
+                                            .setEphemeral(true).queue();
+                                }
+                            }
+                        }
+                    } else {
+                        for (Ticket value : map.values()) {
+                            if (value.getTicketId() == ticketId) {
+                                String userId = value.getUserId();
+                                emb.addField("Ticket ID", ticketId.toString(), false);
+                                emb.addField("User", jda.getUserById(userId).getAsMention(), false);
+                                emb.addField("Topic", value.getTopic(), false);
+                                emb.addField("Message", value.getMessage(), false);
+                                event.replyEmbeds(emb.build()).addActionRow(Button.primary("close", "close ticket"))
+                                        .queue();
+                            }
                         }
                     }
                 } else {
-                    for (Ticket value : map.values()) {
-                        if (value.getSolved() == false) {
-                            tickets.add(value);
+                    if (event.getOption("closeticket") != null) {
+                        event.reply("You must provide a ticket ID.").setEphemeral(true).queue();
+                    } else {
+                        for (Ticket value : map.values()) {
+                            if (value.getSolved() == false) {
+                                tickets.add(value);
+                            }
                         }
+                        for (Ticket ticket : tickets) {
+                            emb.addField(ticket.getTicketId().toString(), ticket.getTopic(), false);
+                        }
+                        emb.setTitle("Open tickets")
+                                .setColor(0xff55ff);
+                        event.replyEmbeds(emb.build()).queue();
                     }
-                    for (Ticket ticket : tickets) {
-                        emb.addField(ticket.getTicketId().toString(), ticket.getTopic(), false);
-                    }
-                    emb.setTitle("Open tickets")
-                            .setColor(0xff55ff);
-                    event.replyEmbeds(emb.build()).setEphemeral(false).queue();
                 }
+            } catch (FileNotFoundException e) {
+                event.reply("There are no tickets avlaible").setEphemeral(true).queue();
+            } catch (ClassCastException e) {
+                event.reply("There are no tickets avlaible").setEphemeral(true).queue();
+            } catch (InvalidDefinitionException e) {
+                event.reply("There are no tickets avlaible").setEphemeral(true).queue();
             } catch (IOException e) {
                 e.printStackTrace();
-            } catch (ClassCastException e) {
-                event.reply("There are no tickets avlaible");
             }
 
         }
@@ -135,14 +166,16 @@ public class support extends ListenerAdapter {
                 ticketId++;
                 lticketId = ticketId;
             }
-            Ticket ticket = new Ticket(false, lticketId, user, topic, message);
+            Ticket ticket = new Ticket(false, lticketId, user.getId(), topic, message);
 
             try {
-                map = mapper.readValue(new File("tickets.json"), new TypeReference<HashMap<Integer, Ticket>>() {
+                map = mapper.readValue(new File("tickets.json"), new TypeReference<Map<Integer, Ticket>>() {
                 });
                 System.out.println(map);
                 map.put(lticketId, ticket);
                 System.out.println(map);
+            } catch (FileNotFoundException e) {
+                map.put(lticketId, ticket);
             } catch (IOException e) {
                 e.printStackTrace();
             }
