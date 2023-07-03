6817e8d3664d3b788c972151a0e93ebea712957a
wyatt_was <127136122+wyattwas@users.noreply.github.com>
2023-07-03 12:33:02 +0000

    Using Modals as new Support Form

--- src/main/java/main/support.java
+++ src/main/java/main/support.java
@@ -6,59 +6,54 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nonnull;
+import javax.security.auth.callback.TextInputCallback;
+import javax.swing.plaf.TextUI;
+
+import org.apache.commons.collections4.map.LRUMap;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.Modal;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import main.ticket;
 
 public class support extends ListenerAdapter {
-    private static Map<Boolean, List<String>> tickets;
-
-    public static void loadTickets() {
-        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("tickets.dat"))) {
-            tickets = (HashMap<Boolean, List<String>>) inputStream.readObject();
-        } catch (FileNotFoundException event) {
-            tickets = new HashMap<>();
-        } catch (IOException | ClassNotFoundException event) {
-            event.printStackTrace();
-        }
-    }
-
-    private void saveTickets() {
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("tickets.dat"))) {
-            outputStream.writeObject(tickets);
-        } catch (IOException event) {
-            event.printStackTrace();
-        }
-    }
+    Integer ticketID;
 
     @Override
     public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
         String command = event.getName();
-        Integer ticketID = 0;
 
-        if (command.equals("support")) {
-            String topic = event.getOption("topic").getAsString();
-            String message = event.getOption("message").getAsString();
-            User user = event.getUser();
-            ticketID++;
+        if (command.equals("ticket")) {
+            TextInput topic = TextInput.create("topic", "Topic", TextInputStyle.SHORT)
+                    .setPlaceholder("Subject of this ticket")
+                    .setMinLength(1)
+                    .setMaxLength(100)
+                    .setRequired(true)
+                    .build();
 
-            new ticket(false, ticketID, user, topic, message);
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(""))
+            TextInput message = TextInput.create("message", "Message", TextInputStyle.PARAGRAPH)
+                    .setPlaceholder("Your message")
+                    .setMinLength(1)
+                    .setMaxLength(1000)
+                    .setRequired(true)
+                    .build();
 
-            event.reply(
-                    "Your Support Form has been submited. You'll be informed when your Form has been processed. Your Ticket has the ID "
-                            + ticketID)
-                    .setEphemeral(true).queue();
+            Modal modal = Modal.create("ticket", "Support Ticket")
+                    .addActionRows(ActionRow.of(topic), ActionRow.of(message))
+                    .build();
         }
 
         if (command.equals("open-ticket")) {
@@ -72,6 +67,30 @@ public class support extends ListenerAdapter {
                 event.reply("It seems that you do not have the necessary authorisation for this action.")
                         .setEphemeral(true).queue();
             }
+        }
+    }
+
+    @Override
+    public void onModalInteraction(@Nonnull ModalInteractionEvent event) {
+        if (event.getModalId().equals("ticket")) {
+            String topic = event.getValue("topic").getAsString();
+            String message = event.getValue("message").getAsString();
+            User user = event.getUser();
+            int lticketId = ticketID + 1;
+            ticketID++;
+
+            try {
+                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("tickets.txt"));
+                out.writeObject(new ticket(false, lticketId, user, topic, message));
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            event.reply(
+                    "Your Support Form has been submited. You'll be informed when your Form has been processed. Your Ticket has the ID "
+                            + ticketID)
+                    .setEphemeral(true).queue();
         }
     }
 }
